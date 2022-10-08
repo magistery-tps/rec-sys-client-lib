a05fa27ba89a6f32e2f0da82551ee15de0d2450c
adrianmarino <adrianmarino@gmail.com>
2022-10-08 15:40:38 -0300

    feat: add sections

--- recsys/recsysweb/urls.py
+++ recsys/recsysweb/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [ 
-    path('', views.home, name='home')   
+    path('', views.home),
+    path('recommendations', views.recommendations),
+    path('likes', views.likes)
 ]
--- recsys/recsysweb/views.py
+++ recsys/recsysweb/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'views/home.html')
+    return render(request, 'pages/home.html')
+
+def recommendations(request):
+    return render(request, 'pages/recommendations.html')
+
+def likes(request):
+    return render(request, 'pages/likes.html')
