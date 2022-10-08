c1cf8c5e29c742c8becc1cc8d0c31c4b644802e2
adrianmarino <adrianmarino@gmail.com>
2022-10-08 16:06:16 -0300

    feat: add sections

--- recsys/recsysweb/urls.py
+++ recsys/recsysweb/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [ 
-    path('', views.home),
-    path('recommendations', views.recommendations),
-    path('likes', views.likes)
+    path('', views.home, name='home'),
+    path('recommendations', views.recommendations, name='recommendations'),
+    path('likes', views.likes, name='likes')
 ]
