de3dfc0d609377ad5d83eb7c980b5cdcf3483290
adrianmarino <adrianmarino@gmail.com>
2022-10-14 20:58:44 -0300

    feat: google sign-in

--- recsys/recsys/settings.py
+++ recsys/recsys/settings.py
@@ -43,8 +43,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+
     'recsysweb',
+
     'rest_framework',
+
+     # Add the following django-allauth apps
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google', # for Google OAuth 2.0
 ]
 
 MIDDLEWARE = [
@@ -157,3 +167,31 @@ REST_FRAMEWORK = {
 
 
 ALLOWED_HOSTS = ['*']
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SITE_ID = 3
+LOGIN_REDIRECT_URL = '/'
+
+# Additional configuration settings
+SOCIALACCOUNT_QUERY_EMAIL = True
+ACCOUNT_LOGOUT_ON_GET= True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_EMAIL_REQUIRED = True
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
--- recsys/recsys/urls.py
+++ recsys/recsys/urls.py
@@ -20,6 +20,7 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('django.contrib.auth.urls')),
     path('', include('recsysweb.urls')),
