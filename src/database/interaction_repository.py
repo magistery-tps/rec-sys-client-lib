7a2b4ee3848beb8dcfa2c088fca0beb473a75ab9
adrianmarino <adrianmarino@gmail.com>
2022-12-23 18:38:44 -0300

    feat: simplify scoring card

--- src/database/interaction_repository.py
+++ src/database/interaction_repository.py
@@ -3,15 +3,17 @@ from .repository import Repository
 
 class InteractionRepository(Repository):
     def __init__(self, database): super().__init__(database)
+
+
     def save(self, row):
         query = """
-            INSERT INTO 
+            INSERT INTO
                 recsys.recsysweb_interaction(item_id, user_id, rating)
             VALUES (
                 ":ITEM_ID",
                 ":USER_ID",
                 ":RATING"
-            );    
+            );
             """.replace(':ITEM_ID', str(int(row['item_id']))) \
                 .replace(':USER_ID', str(int(row['user_id']))) \
                 .replace(':RATING', str(row['rating']))
--- src/database/item_repository.py
+++ src/database/item_repository.py
@@ -5,16 +5,21 @@ class ItemRepository(Repository):
     def __init__(self, database): super().__init__(database)
     def save(self, row):
         query = """
-            INSERT INTO 
-                recsys.recsysweb_item(id, name, description, image)
+            INSERT INTO
+                recsys.recsysweb_item(id, name, description, image, popularity)
             VALUES (
                 :ID,
                 ":NAME",
                 ":DESC",
-                ":IMAGE"
-            );    
+                ":IMAGE",
+                0
+            );
             """.replace(':ID', str(row['id'])) \
                 .replace(':NAME', str(row['name'])) \
                 .replace(':DESC', str(row['description']).replace('"', '')) \
                 .replace(':IMAGE', str(row['image']))
         self.database.execute(query)
+
+
+    def find_all(self):
+        return self.database.execute("SELECT id, name, description, image, popularity FROM recsys.recsysweb_item")
