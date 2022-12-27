0e28dbfba083c4bfc22acfca7e74230f45a7a2be
adrianmarino <adrianmarino@gmail.com>
2022-12-27 00:59:11 -0300

    feat: improve recommendations view

--- recsys/recsysweb/recommender/recommender.py
+++ recsys/recsysweb/recommender/recommender.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class RecommenderContext:
-    def __init__(self, user=None, limit=20, shuffle_limit=100):
+    def __init__(self, user=None, limit=20, shuffle_limit=80):
         self.user          = user
         self.limit         = limit
         self.shuffle_limit = shuffle_limit
