be301b0644b3cf9ca110b819d3a49933c40f2f33
adrianmarino <adrianmarino@gmail.com>
2022-12-27 21:03:31 -0300

    feat: vote only when select a rating value. also order cf recs with similar users ratings only

--- recsys/recsysweb/recommender/cf_recommender.py
+++ recsys/recsysweb/recommender/cf_recommender.py
@@ -3,6 +3,7 @@ from .recommender import Recommender, RecommenderContext
 import random
 from django.db.models import Q
 from ..logger import get_logger
+import numpy as np
 
 class CollaborativeFilteringRecommender(Recommender):
     def __init__(self, config):
@@ -46,27 +47,47 @@ class CollaborativeFilteringRecommender(Recommender):
         return [id_sim[0] for id_sim in id_sim_list]
 
 
-    def __non_seen_similar_user_item_ids(self, user, similar_user_ids):
+    def __non_seen_similar_user_items_mean_rating(self, user, similar_user_ids):
         user_item_ids = [item.item_id for item in Interaction.objects.filter(user=user.id)]
+
 
         similar_user_interactions = []
         for similar_user_id in similar_user_ids:
             similar_user_interactions.extend(Interaction.objects.filter(user=similar_user_id)[:self.__config.max_items_by_similar_user])
 
-        return set([item.item_id for item in similar_user_interactions if item.item_id not in user_item_ids])
+        rating_by_item_id = {}
+        for i in similar_user_interactions:
+            if i.item_id not in user_item_ids:
+                if i.item_id in rating_by_item_id:
+                    rating_by_item_id[i.item_id].append(i.rating)
+                else:
+                    rating_by_item_id[i.item_id] = [i.rating]
+
+        mean_rating_by_item_id = {id: np.mean(ratings) for id, ratings in rating_by_item_id.items()}
+
+        return mean_rating_by_item_id
 
 
     def recommend(self, ctx: RecommenderContext):
         most_similar_user_ids = self.__similar_user_ids(ctx.user)[:self.__config.max_similar_users]
 
-        non_seen_similar_user_item_ids = self.__non_seen_similar_user_item_ids(ctx.user, most_similar_user_ids)
+        mean_rating_by_non_seen_item_id = self.__non_seen_similar_user_items_mean_rating(ctx.user, most_similar_user_ids)
 
-        recommended_items = Item.objects.filter(pk__in=non_seen_similar_user_item_ids).order_by('-popularity')[:ctx.limit]
+        recommended_items = Item.objects.filter(pk__in=mean_rating_by_non_seen_item_id.keys())
 
-        for i in recommended_items:
-            self.logger.info(i)
 
-        return self.__build_result(most_similar_user_ids, non_seen_similar_user_item_ids, recommended_items)
+        recommended_items = sorted(
+            recommended_items,
+            key=lambda x: mean_rating_by_non_seen_item_id[x.id],
+            reverse=True
+        )
+        recommended_items = recommended_items[:ctx.limit]
+
+        # self.logger.info('\n\nRESULT:\n')
+        # for i in recommended_items:
+        #   self.logger.info(f'{i.name}: {mean_rating_by_non_seen_item_id[i.id]}')
+
+        return self.__build_result(most_similar_user_ids, mean_rating_by_non_seen_item_id.keys(), recommended_items)
 
 
     def __build_result(
