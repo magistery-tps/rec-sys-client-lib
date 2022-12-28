647aa77e9f2b8097fb56c1e622757b03d1b234fb
adrianmarino <adrianmarino@gmail.com>
2022-12-28 13:55:15 -0300

    feat: db dump and add many item descriptions

--- recsys/recsysweb/models/item.py
+++ recsys/recsysweb/models/item.py
@@ -4,7 +4,7 @@ from django.db import models
 class Item(models.Model):
     id          = models.AutoField(primary_key=True)
     name        = models.TextField(max_length = 500,  verbose_name = 'Name')
-    description = models.TextField(max_length = 3000, verbose_name = 'Description')
+    description = models.TextField(max_length = 5000, verbose_name = 'Description')
     image       = models.TextField(max_length = 500,  verbose_name = 'Image URL')
     popularity  = models.FloatField(default   = 0,    verbose_name = 'Popularity')
     rating      = models.FloatField(default   = 0,    verbose_name = 'Rating')
