50786a277cb5abdc514ae9e27c52a464bdf11363
adrianmarino <adrianmarino@gmail.com>
2022-12-29 21:04:43 -0300

    feat: see interactions under home view

--- recsys/recsysweb/views/views.py
+++ recsys/recsysweb/views/views.py
@@ -3,6 +3,18 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
+# Domain
+from ..service      import RecommenderService
+
+
+recommender_service = RecommenderService()
+
+
+
 @login_required
 def home(request):
-    return render(request, 'single/home.html')
+    user_n_interactions = recommender_service.n_interactions_by(request.user)
+    response = {
+        'user_n_interactions': user_n_interactions
+    }
+    return render(request, 'single/home.html', response)
