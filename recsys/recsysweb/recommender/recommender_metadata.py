1b55182994f2d729dfb4c5faef639615b1e0f67d
adrianmarino <adrianmarino@gmail.com>
2023-01-07 21:16:32 -0300

    feat: tiny features font

--- recsys/recsysweb/recommender/recommender_metadata.py
+++ recsys/recsysweb/recommender/recommender_metadata.py
@@ -2,7 +2,7 @@ class RecommenderMetadata:
     def __init__(self, id, name, title, description, features='', position=0):
         self.id          = id
         self.name        = name
+        self.title       = title
         self.features    = features
-        self.title       = f'{title } ({self.features})' if features else title
         self.description = description
         self.position    = position
